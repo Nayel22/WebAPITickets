e8e0fe9b007fc72897d5beb28782893e30cde5c5
Nayel22 <nayelsolorzanom213@gmail.com>
2025-03-29 17:54:58 -0600

    RolesController completado, ahora toca proseguir con usuario

--- Controllers/RolesController.cs
+++ Controllers/RolesController.cs
@@ -17,9 +17,8 @@ namespace WebAPITickets.Controllers
 
             _contexto = contexto;
 
-
-
         }
+
         [HttpGet]
 
         public async Task<ActionResult<IEnumerable<Roles>>> GetRol()
@@ -30,5 +29,81 @@ namespace WebAPITickets.Controllers
 
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Roles>> GetRolById(int id)
+        {
+            var rol = await _contexto.Roles.FindAsync(id);
+
+            if (rol == null)
+            {
+                return NotFound();
+            }
+
+            return rol;
+        }
+
+        [HttpPost]
+        [Route("crear")]
+        public async Task<ActionResult<Roles>> PostRol([FromBody] Roles rol)
+        {
+            _contexto.Roles.Add(rol);
+            await _contexto.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetRolById), new { id = rol.ro_identificador }, rol);
+        }
+
+
+        [HttpPut]
+        [Route("actualizar/{id}")]
+        public async Task<IActionResult> PutRol(int id, [FromBody] Roles rol)
+        {
+            if (id != rol.ro_identificador)
+            {
+                return BadRequest();
+            }
+
+            _contexto.Entry(rol).State = EntityState.Modified;
+
+            try
+            {
+                await _contexto.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_contexto.Roles.Any(e => e.ro_identificador == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+        public async Task<IActionResult> DeleteRol(int id)
+        {
+            var rol = await _contexto.Roles.FindAsync(id);
+            if (rol == null)
+            {
+                return NotFound();
+            }
+
+            _contexto.Roles.Remove(rol);
+            await _contexto.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+
+
     }
+
+
 }
+
