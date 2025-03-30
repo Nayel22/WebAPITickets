5bf43e4bc88b8962e96905e3682e54a4b72b93ba
Nayel22 <nayelsolorzanom213@gmail.com>
2025-03-29 18:09:39 -0600

    UsuariosController

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,14 @@
+﻿using WebAPITickets.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPITickets.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
