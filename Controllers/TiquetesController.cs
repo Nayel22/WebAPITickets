a19451ff387b72f7b9e3705f5f2f59eb6be31c44
Nayel22 <nayelsolorzanom213@gmail.com>
2025-03-29 19:09:20 -0600

    se agrego TiqutesController

---
+++ Controllers/TiquetesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPITickets.Controllers
+{
+    public class TiquetesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
