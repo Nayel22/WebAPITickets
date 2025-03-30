eb2f295652376c6748af626e315de55ea66abd86
Nayel22 <nayelsolorzanom213@gmail.com>
2025-03-29 18:34:00 -0600

    UsuariosController terminado

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -1,14 +1,94 @@
-﻿using WebAPITickets.Models;
-
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPITickets.Database;
+using WebAPITickets.Models;
 
 namespace WebAPITickets.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class UsuariosController : Controller
     {
-        public IActionResult Index()
+        private readonly ContextoBD _contexto;
+
+        public UsuariosController(ContextoBD contexto)
         {
-            return View();
+            _contexto = contexto;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Usuarios>>> GetUsuarios()
+        {
+            return await _contexto.Usuarios.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Usuarios>> GetUsuarioById(int id)
+        {
+            var usuario = await _contexto.Usuarios.FindAsync(id);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return usuario;
+        }
+
+        [HttpPost]
+        [Route("crear")]
+        public async Task<ActionResult<Usuarios>> PostUsuario([FromBody] Usuarios usuario)
+        {
+            _contexto.Usuarios.Add(usuario);
+            await _contexto.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetUsuarioById), new { id = usuario.us_identificador }, usuario);
+        }
+
+        [HttpPut]
+        [Route("actualizar/{id}")]
+        public async Task<IActionResult> PutUsuario(int id, [FromBody] Usuarios usuario)
+        {
+            if (id != usuario.us_identificador)
+            {
+                return BadRequest();
+            }
+
+            _contexto.Entry(usuario).State = EntityState.Modified;
+
+            try
+            {
+                await _contexto.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_contexto.Usuarios.Any(e => e.us_identificador == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+        public async Task<IActionResult> DeleteUsuario(int id)
+        {
+            var usuario = await _contexto.Usuarios.FindAsync(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            _contexto.Usuarios.Remove(usuario);
+            await _contexto.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
--- Database/ContextoBD.cs
+++ Database/ContextoBD.cs
@@ -8,12 +8,22 @@ namespace WebAPITickets.Database
         public ContextoBD(DbContextOptions<ContextoBD> options) : base(options) { }
 
         public DbSet<Roles> Roles { get; set; }
+        public DbSet<Usuarios> Usuarios { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
 
         {
 
             modelBuilder.Entity<Roles>().HasKey(x => x.ro_identificador);
+            modelBuilder.Entity<Usuarios>().HasKey(x => x.us_identificador);
+
+
+            // Configura la relación entre Usuarios y Roles
+            modelBuilder.Entity<Usuarios>()
+                .HasOne(u => u.Rol)
+                .WithMany()
+                .HasForeignKey(u => u.us_ro_identificador);
 
         }
 
