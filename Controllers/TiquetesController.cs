e22ed6f65f7bb7d5535fea8213cec4277f7360f9
Nayel22 <nayelsolorzanom213@gmail.com>
2025-03-29 19:49:05 -0600

    TiquetesController

--- Controllers/TiquetesController.cs
+++ Controllers/TiquetesController.cs
@@ -1,12 +1,94 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPITickets.Database;
+using WebAPITickets.Models;
 
 namespace WebAPITickets.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class TiquetesController : Controller
     {
-        public IActionResult Index()
+        private readonly ContextoBD _contexto;
+
+        public TiquetesController(ContextoBD contexto)
         {
-            return View();
+            _contexto = contexto;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tiquetes>>> GetTiquetes()
+        {
+            return await _contexto.Tiquetes.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Tiquetes>> GetTiqueteById(int id)
+        {
+            var tiquete = await _contexto.Tiquetes.FindAsync(id);
+
+            if (tiquete == null)
+            {
+                return NotFound();
+            }
+
+            return tiquete;
+        }
+
+        [HttpPost]
+        [Route("crear")]
+        public async Task<ActionResult<Tiquetes>> PostTiquete([FromBody] Tiquetes tiquete)
+        {
+            _contexto.Tiquetes.Add(tiquete);
+            await _contexto.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTiqueteById), new { id = tiquete.ti_identificador }, tiquete);
+        }
+
+        [HttpPut]
+        [Route("actualizar/{id}")]
+        public async Task<IActionResult> PutTiquete(int id, [FromBody] Tiquetes tiquete)
+        {
+            if (id != tiquete.ti_identificador)
+            {
+                return BadRequest();
+            }
+
+            _contexto.Entry(tiquete).State = EntityState.Modified;
+
+            try
+            {
+                await _contexto.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_contexto.Tiquetes.Any(e => e.ti_identificador == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+        public async Task<IActionResult> DeleteTiquete(int id)
+        {
+            var tiquete = await _contexto.Tiquetes.FindAsync(id);
+            if (tiquete == null)
+            {
+                return NotFound();
+            }
+
+            _contexto.Tiquetes.Remove(tiquete);
+            await _contexto.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
--- Database/ContextoBD.cs
+++ Database/ContextoBD.cs
@@ -9,6 +9,7 @@ namespace WebAPITickets.Database
 
         public DbSet<Roles> Roles { get; set; }
         public DbSet<Usuarios> Usuarios { get; set; }
+        public DbSet<Tiquetes> Tiquetes { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -17,6 +18,7 @@ namespace WebAPITickets.Database
 
             modelBuilder.Entity<Roles>().HasKey(x => x.ro_identificador);
             modelBuilder.Entity<Usuarios>().HasKey(x => x.us_identificador);
+            modelBuilder.Entity<Tiquetes>().HasKey(x => x.ti_identificador);
 
 
             // Configura la relación entre Usuarios y Roles
@@ -25,6 +27,13 @@ namespace WebAPITickets.Database
                 .WithMany()
                 .HasForeignKey(u => u.us_ro_identificador);
 
+
+
+            // Configura la relación entre Tiquetes y Usuarios
+            modelBuilder.Entity<Tiquetes>()
+                .HasOne(t => t.UsuarioAsignado)
+                .WithMany()
+                .HasForeignKey(t => t.ti_us_id_asigna);
         }
 
     }
